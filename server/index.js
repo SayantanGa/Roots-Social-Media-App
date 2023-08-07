756d1b2e9f2099eb868c907fa2e65590c4f9c819
SayantanGa <103838076+SayantanGa@users.noreply.github.com>
2023-08-07 09:57:05 +0530

    fixed CORS

--- server/index.js
+++ server/index.js
@@ -8,37 +8,14 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(cors());
+app.options("*", cors());
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(`/api/v1/posts`, postRouter);
 app.use(`/api/v1/users`, userRouter);
-/*
-app.use(cors({
-  origin: ['https://roots-social-media-app-api.onrender.com', 'https://roots-social-media-app.vercel.app'],
-  methods: ['GET', 'POST', 'DELETE', 'PUT', 'PATCH'],
-  credentials: true
-}));
-app.options('*', cors());
-*/
-
-app.use(function (req, res, next) {
-  const allowedOrigins = [
-    "https://roots-social-media-app-api.onrender.com",
-    "https://roots-social-media-app.vercel.app",
-  ];
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.header("Access-Control-Allow-credentials", true);
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
-  next();
-});
 
 module.exports = app;
