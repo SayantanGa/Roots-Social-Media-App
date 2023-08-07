ae0b3d036e645ab1d3eb341065ba94d3559fcbcf
SayantanGa <103838076+SayantanGa@users.noreply.github.com>
2023-08-07 18:36:02 +0530

    fix

--- server/index.js
+++ server/index.js
@@ -1,33 +1,25 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-const helmet = require('helmet');
+const helmet = require("helmet");
 const postRouter = require("./routes/postRoutes");
 const userRouter = require("./routes/userRoutes");
 const cookieParser = require("cookie-parser");
-const bodyParser = require("body-parser");
-const compression = require('compression');
+const compression = require("compression");
 
 const app = express();
-app.enable('trust proxy');
+app.enable("trust proxy");
 
 app.use(cors());
 app.options("*", cors());
 
-
 app.use(express.static(path.join(__dirname, "public")));
 app.use(helmet());
-app.post(
-    '/webhook-checkout',
-    bodyParser.raw({ type: 'application/json' }),
-    bookingController.webhookCheckout
-  );
-  app.use(express.json({ limit: '10kb' }));
-app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+app.use(express.json({ limit: "10kb" }));
+app.use(express.urlencoded({ extended: true, limit: "10kb" }));
 app.use(cookieParser());
 app.use(compression());
 app.use(`/api/v1/posts`, postRouter);
 app.use(`/api/v1/users`, userRouter);
-
 
 module.exports = app;
